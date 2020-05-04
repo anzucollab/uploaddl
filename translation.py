f9c100bcbfc74b31a1d9766a9f063bfe4b026e4d
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-04 18:20:23 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """ **Hi,** **Welcome** **To** **@DLRename_Bot**
+    START_TEXT = """ **Hi,** **Welcome** **To** **@MultiDL_Bot**
 **/help** **to** **Know** **how** **to** **use** **me..!!**
 **My** **Credits** **to** **:** **@Amal_PM**
  **For** **More** **Better** **Use**
