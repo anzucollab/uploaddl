56116722dd8430659d9637392777dbc462913b4a
ShinchanNohara1 <72728580+ShinchanNohara1@users.noreply.github.com>
2020-12-14 23:28:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,33 +19,6 @@ else:
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-LOGGER = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-
-ENV = bool(os.environ.get('ENV', False))
-try:
-  if ENV:
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    APP_ID = os.environ.get('APP_ID')
-    API_HASH = os.environ.get('API_HASH')
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    SUDO_USERS = os.environ.get('SUDO_USERS')
-    SUPPORT_CHAT_LINK = os.environ.get('SUPPORT_CHAT_LINK', 'https://t.me/ViperCommunity')
-    DOWNLOAD_DIRECTORY = os.environ.get("DOWNLOAD_DIRECTORY", "./downloads/")
-  else:
-    from bot.config import config
-    BOT_TOKEN = config.BOT_TOKEN
-    APP_ID = config.APP_ID
-    API_HASH = config.API_HASH
-    DATABASE_URL = config.DATABASE_URL
-    SUDO_USERS = config.SUDO_USERS
-    SUPPORT_CHAT_LINK = config.SUPPORT_CHAT_LINK
-    DOWNLOAD_DIRECTORY = config.DOWNLOAD_DIRECTORY
-except KeyError:
-  LOGGER.error('One or more configuration values are missing exiting now.')
-  exit(1)
-
 
 if __name__ == "__main__" :
     # create download directory, if not exist
@@ -55,13 +28,9 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = pyrogram.Client(
-        "AnyDLBot",
-        bot_token=Config.TG_BOT_TOKEN,
+        session_name=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins
-        parse_mode="markdown",
-        workdir=DOWNLOAD_DIRECTORY
     )
-    Config.AUTH_USERS.add(7351948)
     app.run()
