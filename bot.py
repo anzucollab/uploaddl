078b8f0fa2798ee8585ffe452391b7fb2f8da6c4
ShinchanNohara1 <72728580+ShinchanNohara1@users.noreply.github.com>
2020-12-14 21:15:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,33 @@ else:
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+LOGGER = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+ENV = bool(os.environ.get('ENV', False))
+try:
+  if ENV:
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    APP_ID = os.environ.get('APP_ID')
+    API_HASH = os.environ.get('API_HASH')
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    SUDO_USERS = os.environ.get('SUDO_USERS')
+    SUPPORT_CHAT_LINK = os.environ.get('SUPPORT_CHAT_LINK', 'https://t.me/ViperCommunity')
+    DOWNLOAD_DIRECTORY = os.environ.get("DOWNLOAD_DIRECTORY", "./downloads/")
+  else:
+    from bot.config import config
+    BOT_TOKEN = config.BOT_TOKEN
+    APP_ID = config.APP_ID
+    API_HASH = config.API_HASH
+    DATABASE_URL = config.DATABASE_URL
+    SUDO_USERS = config.SUDO_USERS
+    SUPPORT_CHAT_LINK = config.SUPPORT_CHAT_LINK
+    DOWNLOAD_DIRECTORY = config.DOWNLOAD_DIRECTORY
+except KeyError:
+  LOGGER.error('One or more configuration values are missing exiting now.')
+  exit(1)
+
 
 if __name__ == "__main__" :
     # create download directory, if not exist
