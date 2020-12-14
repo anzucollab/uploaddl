e111f2aff7a766ba27d117fd1e57bb5891586462
ShinchanNohara1 <72728580+ShinchanNohara1@users.noreply.github.com>
2020-12-14 20:47:22 +0530

    Update authorize.py

--- plugins/authorize.py
+++ plugins/authorize.py
@@ -2,7 +2,7 @@ import re
 import json
 from httplib2 import Http
 from bot.config import Messages
-from pyrogram import Client, filters
+from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from oauth2client.client import OAuth2WebServerFlow, FlowExchangeError
 from googleapiclient.discovery import build
