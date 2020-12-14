74ba3ca12441567f7ad629c669d42e1e2d87c3db
ShinchanNohara1 <72728580+ShinchanNohara1@users.noreply.github.com>
2020-12-14 12:12:57 +0530

    Update _core.py

--- plugins/_core.py
+++ plugins/_core.py
@@ -376,8 +376,8 @@ def button(bot, update):
                     duration = metadata.get('duration').seconds
             # get the correct width, height, and duration for videos greater than 10MB
             if os.path.exists(thumb_image_path):
-                width = 0
-                height = 0
+                width = 30
+                height = 30
                 metadata = extractMetadata(createParser(thumb_image_path))
                 if metadata.has("width"):
                     width = metadata.get("width")
